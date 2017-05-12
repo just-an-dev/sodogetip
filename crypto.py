f94da7c6bff810c730b47723f0cd2042761b91ca
just-an-dev <just-an-dev@ggcorp.fr>
2017-05-12 22:20:00 +0200

    unspend require a least 1 confirmation

--- crypto.py
+++ crypto.py
@@ -34,7 +34,7 @@ def tip_user(rpc, sender_user, receiver_user, amount_tip):
 def send_to(rpc, sender_address, receiver_address, amount, take_fee_on_amount=False):
     print "send " + amount + " to " + receiver_address + " from " + sender_address
 
-    list_unspent = rpc.listunspent(0, 99999999999, [sender_address])
+    list_unspent = rpc.listunspent(1, 99999999999, [sender_address])
 
     unspent_list = []
     unspent_vout = []
