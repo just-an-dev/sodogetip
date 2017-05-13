2d35ee3d5cd07765fe98b2a369ddf881d9244906
just-an-dev <just-an-dev@ggcorp.fr>
2017-05-14 01:37:27 +0200

    close issue #15 reduce praw logs

--- bot_logger.py
+++ bot_logger.py
@@ -4,16 +4,19 @@ from logging.handlers import RotatingFileHandler
 
 from config import DATA_PATH, bot_config
 
-if not os.path.exists(DATA_PATH+bot_config['logs_path']):
-    os.makedirs(DATA_PATH+bot_config['logs_path'])
+if not os.path.exists(DATA_PATH + bot_config['logs_path']):
+    os.makedirs(DATA_PATH + bot_config['logs_path'])
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
+logging.getLogger('requests').setLevel(logging.WARNING)
+logging.getLogger('prawcore').setLevel(logging.WARNING)
+
 formatter_file = logging.Formatter('%(asctime)s :: %(levelname)s :: %(name)s :: %(pathname)s:%(lineno)s :: %(message)s')
 formatter_output = logging.Formatter('%(asctime)s -  - %(levelname)s - %(message)s')
 
-file_handler = RotatingFileHandler(DATA_PATH+bot_config['logs_path']+'activity.log', 'a', 1000000, 1)
+file_handler = RotatingFileHandler(DATA_PATH + bot_config['logs_path'] + 'activity.log', 'a', 1000000, 1)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter_file)
 logger.addHandler(file_handler)
@@ -22,6 +25,3 @@ steam_handler = logging.StreamHandler()
 steam_handler.setLevel(logging.DEBUG)
 steam_handler.setFormatter(formatter_output)
 logger.addHandler(steam_handler)
-
-logger_praw = logging.getLogger('prawcore')
-logger_praw.setLevel(logging.WARNING)
