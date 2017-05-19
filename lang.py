53fc88a26abef2856ba5fddd825f91f76a89eba6
Gary Littlemore <GaryLittlemore@users.noreply.github.com>
2017-05-19 22:48:08 +0100

    Update lang.py

--- lang.py
+++ lang.py
@@ -1,6 +1,6 @@
 link_register = '[register](http://www.reddit.com/message/compose?to=sodogetiptest&subject=%2Bregister&message=%2Bregister)'
 
-message_register_success = "%s registered, your address is %s"
+message_register_success = "%s your account is registered, your wallet address is %s"
 message_need_register = 'You need %s before' % link_register
 message_invalid_amount = 'You must use valid amount'
 message_balance_low_tip = '+/u/%s your balance is too low for this tip'
