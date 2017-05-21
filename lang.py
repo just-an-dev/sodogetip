0f40b05bedf962832d55fe925d5bf1ac429dd462
Gary Littlemore <GaryLittlemore@users.noreply.github.com>
2017-05-21 16:55:53 +0100

    Update lang.py
    More changes to lang.py

--- lang.py
+++ lang.py
@@ -3,19 +3,19 @@ link_register = '[register](http://www.reddit.com/message/compose?to=sodogetipte
 message_register_success = "%s your account is registered, your wallet address is %s"
 message_need_register = 'You need %s before' % link_register
 message_invalid_amount = 'You must use valid amount'
-message_balance_low_tip = '+/u/%s your balance is too low for this tip'
+message_balance_low_tip = '+/u/%s your wallet balance is too low for this tip'
 message_already_registered = 'You are already registered '
-message_balance_low_withdraw = '+/u/%s your balance is too low for this withdraw '
+message_balance_low_withdraw = '+/u/%s your wallet balance is too low for this withdraw '
 message_info = '\n\nHere are your account details %s!' \
                '\n\n^very ^info |&nbsp;' \
                '\n---|---' \
-               '\n^Your ^Balance|%s DOGE' \
+               '\n^Your ^Balance|%s doge' \
                '\n^Deposit ^Address|%s' \
                '\n^Withdraw | ^[+withdraw](https://www.reddit.com/message/compose?to=sodogetiptest&subject=%2Bwithdraw&message=%2Bwithdraw%20AMOUNT%20doge%20to%20ADDRESS)'
 message_not_supported = 'Currently not supported'
-message_balance = 'Your balance is: %s ( %s$ )'
+message_balance = 'Your wallet balance is: %s doge ( $%s )'
 message_history = 'Your history: \n\n'
-message_tip = '+/u/%s tip %s to %s'
+message_tip = '+/u/%s %s doge --> +/u/%s'
 message_withdraw = 'Withdraw : %s to %s'
 message_footer = '\n\n*****' \
                 '\n\nNew to Dogecoin or sodogetip? Ask the community any questions on /r/dogecoin!' \
