a6db5af25f62bae540c86af92d103c45896e9743
PowerLemons <f0kent0sh@gmail.com>
2017-05-26 20:17:17 -0300

    Formatting typo fix in message_recipient_register

--- lang.py
+++ lang.py
@@ -62,7 +62,7 @@ message_help = "To tip someone: \n\n Reply to their comment or post with: +/u/" 
                "\n\nReplace 100 with whatever amount you want to tip" \
                "\n\n*****"
 
-message_recipient_register = Template("__^[such ^error]__: ^/u/{{ username }} ^needs ^to ^[register](" + link_register + ") ^before ^receiving ^any ^tips ^(this ^tip ^has ^been ^saved ^for ^3 ^days) ^[[help]](" + link_help + ")")
+message_recipient_register = Template("__^[such ^error]__: ^/u/{{ username }} ^needs ^to ^[register](" + link_register + ") ^before ^receiving ^any ^tips. __^\(this ^tip ^has ^been ^saved ^for ^3 ^days)__ ^[[help]](" + link_help + ")")
 message_recipient_need_register_title = Template("Someone sent you a Dogecoin tip of Ð{{ amount }}, and you need to register to receive it!")
 message_recipient_need_register_message =  Template("Hello {{ username }}! You need to register an account before you can receive __{{ sender }}\"s__ Dogecoin tip of __Ð{{ amount }} doge (${{ value_usd }})__." \
                         "\n\nTo register an account:" \
