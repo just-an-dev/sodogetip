a0f420d13f33dc7f9927184ecebffe924d1c6652
PowerLemons <f0kent0sh@gmail.com>
2017-05-27 16:29:01 -0300

    +history formatting change and fix

--- bot_command.py
+++ bot_command.py
@@ -152,8 +152,8 @@ def tip_user(rpc, reddit, msg):
 def history_user(msg):
     if user_function.user_exist(msg.author.name):
         data = user_function.get_user_history(msg.author.name)
-        history_table = "Date|Sender|Receiver|Amount|Action|Finish|\n"
-        history_table += ":-:|:-:|:-:|:-:|:-:|:-:\n"
+        history_table = "\n\nDate|Sender|Receiver|Amount|Action|Finish|\n"
+        history_table += "---|---|---|---|:-:|:-:\n"
         for tip in data:
             history_table += "%s|%s|%s|%s|%s|%s|\n" % (
                 datetime.datetime.strptime(tip['time'], '%Y-%m-%dT%H:%M:%S.%f'), tip['sender'], tip['receiver'],
