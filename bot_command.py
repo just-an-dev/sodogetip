f51baffb36966280a1dccf72cd72c9b07465d82a
just-an-dev <just-an-dev@users.noreply.github.com>
2017-06-03 20:19:59 +0200

    Update bot_command.py
    add str for config.bot_name

--- bot_command.py
+++ bot_command.py
@@ -124,9 +124,9 @@ def withdraw_user(rpc, msg):
 def tip_user(rpc, reddit, msg):
     bot_logger.logger.info('An user mention detected ')
     split_message = msg.body.lower().strip().split()
-    tip_index = split_message.index('+/u/' + config.bot_name)
+    tip_index = split_message.index(str('+/u/' + config.bot_name))
 
-    if split_message[tip_index] == ('+/u/' + config.bot_name) and split_message[tip_index + 2] == 'doge':
+    if split_message[tip_index] == str('+/u/' + config.bot_name) and split_message[tip_index + 2] == 'doge':
         amount = split_message[tip_index + 1]
         value_usd = utils.get_coin_value(amount)
 
