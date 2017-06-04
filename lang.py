9b372d795187c4b4ad7f3cbfe184dd4a670d64d9
peoplma <peoplma@gmail.com>
2017-06-04 12:48:14 -0400

    Update lang.py

--- lang.py
+++ lang.py
@@ -73,3 +73,4 @@ message_recipient_need_register_message =  ("Hello /u/{{ username }}! You need t
                         "\n\n2. Click the \"Send\" button" \
                         "\n\n3. Receive the successful register message" \
                         "\n\nThe successful register message will contain your Dogecoin address to your tipping account.")
+message_tipping yourself = ("__^[such ^error]__: ^wow ^plz ^no ^tip ^self")
