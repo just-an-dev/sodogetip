dc40e3f6a794c3f8a26ce4a67e07eee2220761db
just-an-dev <just-an-dev@ggcorp.fr>
2017-08-13 01:14:41 +0200

    fix issue #79

---
+++ scripts/check_gold.py
@@ -0,0 +1,17 @@
+import time
+
+import praw
+
+import config
+
+operator_user = "just-an-dev"
+
+while True:
+    reddit = praw.Reddit(config.bot_config)
+    if int(reddit.user.me().gold_creddits) < 1:
+        print ("Send message to operator")
+
+        reddit.redditor(operator_user).message('No more Gold', "No More Gold on Bot to Sell !! ")
+
+    # each 30 minutes
+    time.sleep(30 * 60)
