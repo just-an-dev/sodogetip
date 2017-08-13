ca4051379545d386ceef11308b9f2b03a5b11dc3
just-an-dev <just-an-dev@ggcorp.fr>
2017-08-13 23:23:40 +0200

    add getnewaddress to mock rpc

--- MockRpc.py
+++ MockRpc.py
@@ -5,3 +5,7 @@ class MockRpc:
                 "pubkey": "02cacc9e6cfcbb0b2a23569838ad908193a9a1456911fe45c4fff7f9daa06b8809",
                 "scriptPubKey": "76a914ce2100bf6580b586dba3c0d2df0f2196940d2f4588ac"}
         return data
+
+    def getnewaddress(self, account=None):
+        data = "nnBKn39onxAuS1cr6KuLAoV2SdfFh1dpsR"
+        return data
